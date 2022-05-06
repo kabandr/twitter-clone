7bb11861d396cb61b2442314ccb28028d2c25a6c
Rene Kabanda <2495191+kabandr@users.noreply.github.com>
2022-05-06 14:58:57 +0100

    create starter project with next.js & tailwind css

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter Clone</title>
+      </Head>
+
+      <h1>Hello World</h1>
+
+    </div>
+  )
+}
+
+export default Home
