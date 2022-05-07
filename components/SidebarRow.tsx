0080e99e06c0ca0ee2710173f0daf81851fee504
root <root@DESKTOP-H7492GR.localdomain>
2022-05-07 22:43:32 +0100

    feat: added sidebar component

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -3,13 +3,17 @@ import React, { SVGProps } from 'react'
 interface Props {
     Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
     title: string
+    onClick?: () => {}
 }
 
-function SidebarRow({ Icon, title }: Props) {
+function SidebarRow({ Icon, title, onClick }: Props) {
     return (
-        <div className='flex items-center space-x-2 rounded-full px-4 py-3 hover:bg-gray-100 transition-all duration-200 group'>
+        <div onClick={() => onClick?.()} className='group flex max-w-fit cursor-pointer 
+        items-center space-x-2 rounded-full 
+        px-4 py-3 hover:bg-gray-100 transition-all duration-200'>
             <Icon className="h-6 w-6" />
-            <p className='group-hover:text-twitter'>{title}</p>
+            <p className='hidden md:inline-flex 
+            group-hover:text-twitter text-base font-light lg:text-xl'>{title}</p>
         </div>
     )
 }
